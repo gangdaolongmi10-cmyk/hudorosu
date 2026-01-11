c00210064ff5645717e9ba13c7b4e7cea281af71
okajima <r_okajima@lermo.co.jp>
2026-01-11 15:18:35 +0900

    警告 修正

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
+    plugins: [react(), tsconfigPaths({
+        ignoreConfigErrors: true
+    })],
     server: {
         host: '0.0.0.0',
         port: 5173,
